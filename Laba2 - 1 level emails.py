59129e0e5143f44959fece3d1524dd7bf4954771
NikolayTV <32233366+NikolayTV@users.noreply.github.com>
2017-10-24 12:24:29 +0700

    Update Laba2 - 1 level emails.py

--- Laba2 - 1 level emails.py
+++ Laba2 - 1 level emails.py
@@ -1,47 +1,58 @@
-import requests                   #выполняет HTTP-запросы
-from bs4 import BeautifulSoup     #работа с HTML
-import csv
+import requests
+from bs4 import BeautifulSoup as BS
 import re
-#работа с форматом данных CSV
-from multiprocessing import Pool  #предоставляет возможность параллельных процессов
 
-links = []
-html_page = requests.get('http://www.mosigra.ru/').text
-soup = BeautifulSoup(html_page, 'lxml')
-href = soup.find_all('a', {'href': re.compile('.+')})
-for tag in href:
-    link = tag['href']
-    links.append(link)
-print(links)
+def get_links(stock):
+    write_links = open('ALL_LINKS.txt', 'a', encoding='utf-8')
+    for x in stock:
+        new_links = []
+        text = requests.get(x).text
+        href = BS(text, 'lxml').find_all('a', {'href': re.compile('.+')})
 
-found = []
-found1 = []
-found2 = []
-found3 = []
+        for tag in href:
+            link = tag['href']
+            if link[0:13] == '//www.mosigra' and link[-1] == '/' and link[-8:0] != '2017/04/':
+                #первое условие чтобы ссылка была с головой
+                #второе условия нужно чтобы отсечь ссылки типа //ufa.mosigra.ru/?local где сайт копируется на другой город
+                #третье условие для отсечения бесконечного календаря
+                new_links.append('http:' + link)
+            elif link[0] == '/' and link[-1] == '/' and link[-8:0] != '2017/04/':
+                new_links.append('http://mosigra.ru' + link)
+        new_adr = []
+        for new_link in new_links:
+            if new_link not in stock:
+                new_adr.append(new_link)
+                write_links.write(new_link + '\n')
+                stock.append(new_link)
+                new_adr.append(new_link)
+        print('len(stock)', len(stock))            #количество общего списка ссылок
+        print('len(new_links)', len(new_links))    #количество ссылок на странице
+        print('len(new_adr)', len(new_adr))        #количество уникальных ссылок на странице
 
-mailsrch = re.compile(r'[\w.][\w.]+@[\w][\w\.]+[a-zA-Z]{1,4}')
+    write_links.close()
+    get_links(stock)
 
-for link in links:
-    if link[0:3] == '//w':
-        site = requests.get('http:' + link).text.split(' ')
-        for line in site:
-            found1 += re.findall(mailsrch, line)
+def get_emails(file_name):
+    read_file = open(file_name, 'r', encoding='utf-8')
+    temp = read_file.read().splitlines()
+    emails = []
+    i = 0
+    for x in temp:
+        i += 1
+        print(i)
+        for line in requests.get(x).text.split(' '):
+            emails += re.findall(re.compile(r'[\w.][\w.]+@[\w][\w\.]+[a-zA-Z]{1,4}'), line)
 
-    elif link[0:2] == '//':
-        site = requests.get('http:' + link).text.split(' ')
-        for line in site:
-            found2 += re.findall(mailsrch, line)
+        unic_emails = set(emails[:])
+        print('len(unic_emails)', len(unic_emails)) #количество уникальных емайлов
+        print('len(emails)', len(emails))           #количество всего емайлов
 
-    elif link[0] == '/':
-        site = requests.get('http://mosigra.ru'+link).text.split(' ')
-        for line in site:
-            found3 += re.findall(mailsrch, line)
-    else:
-        pass
+    file_email = open('ALL_emails.txt', 'w', encoding='utf-8')  # создает и выгружает емайлы в файл
+    file_email.write('\n'.join(unic_emails))
+    file_email.close()
+    print('Файл с емайлами успешно создан')
+    read_file.close()
 
-    found = found1 + found2 + found3
-    found = set(found)
-    print(len(found))
-    print(found)
-
+get_links(['http://www.mosigra.ru/'])
+get_emails('ALL_LINKS.txt')
 
